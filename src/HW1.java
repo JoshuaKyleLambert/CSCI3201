36ba77ceacbecac3eed1597b9f415438a2616edc
Joshua <jl6356@stu.armstrong.edu>
2017-08-17 13:20:51 -0400

    Final update, No leading zeros on the binary...

--- src/HW1.java
+++ src/HW1.java
@@ -2,7 +2,7 @@
  * Created by Joshua Lambert on 8/16/2017.
  * CSCI3201 HW1 Binary WarmUP
  */
-public class BinaryWarmUp {
+public class HW1 {
         public static void main(String[] args) {
                 System.out.format("DECI   BIN  HEX%n");
             for ( int i = 0; i < 16 ; i++) {
